f63ded545ac1596e142af88d150e41d7fc3f0bb5
krasikov-ilya <77252835+krasikov-ilya@users.noreply.github.com>
2024-11-12 15:29:03 -0500

    Java Spring Starter Project
    Maven project, spring boot version 3.3.5, Java Version 23. Dependencies: Spring Web, MySQL Driver

---
+++ ecomproject/demo/src/test/java/com/eecs4413final/demo/VGameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eecs4413final.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VGameStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
