c8326b5398e6eb56d8a54028371b517ffd118731
Shabbir1065 <shabbir1065@gmail.com>
2024-12-16 21:53:36 -0500

    did the same thing in backend (made change to WebConfig so we reference a .env variable instead of a real link)

--- backend/ecomproject/demo/src/main/java/com/eecs4413final/demo/config/WebConfig.java
+++ backend/ecomproject/demo/src/main/java/com/eecs4413final/demo/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.eecs4413final.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    @Value("${ALLOWED_ORIGIN}")
+    private String allowedOrigin;
+
+    public void addCorsMappings(CorsRegistry registry, @Value("${ALLOWED_ORIGIN}") String allowedOrigin) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") 
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type") 
                 .allowCredentials(true); 
